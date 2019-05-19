36b07c1b2518f9be84a9b750ac3784620ece645f
Asadullah Al Hamasy <33560914+AlHamasy@users.noreply.github.com>
2019-05-19 20:36:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,14 +27,14 @@ include 'koneksi.php';
 			$query = $conn->prepare("SELECT * FROM datasiswa");
 			$query -> execute();
 		
-			$row = $query->fetchObject();
+			//$row = $query->fetchObject();
 			
  			while ($row = $query->fetchObject()) {
 				
   				echo "<tr>";
-  				echo "<td>".$row->NIM."</td>";
-  				echo "<td>".$row->NAMA."</td>";
- 				echo "<td>".($row->JENKEL == "L" ? "Laki-laki" : "Perempuan")."</td>";
+  				echo "<td>".$row['nim']."</td>";
+  				echo "<td>".$row['nama']."</td>";
+ 				echo "<td>".($row->['jenkel'] == "L" ? "Laki-laki" : "Perempuan")."</td>";
   				echo "<td><a href='delete.php?nim=".$row['nim']."'>delete</a></td>";
   				echo "<td><a href='update.php?nim=".$row['nim']."'>update</a></td>";
   				echo "<tr>";
